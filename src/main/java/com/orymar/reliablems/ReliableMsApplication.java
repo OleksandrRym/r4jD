2b5c3f447e4371d070115327be8474886a8298b4
OleksandrRym <lostmannight@gmail.com>
2026-08-08 22:35:21 +0300

    add docker-compose.yml

--- src/main/java/com/orymar/reliablems/ReliableMsApplication.java
+++ src/main/java/com/orymar/reliablems/ReliableMsApplication.java
@@ -2,8 +2,12 @@ package com.orymar.reliablems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@Configuration
+@EnableKafka
 public class ReliableMsApplication {
 
     public static void main(String[] args) {
--- src/main/resources/static/load_test.js
+++ src/main/resources/static/load_test.js
@@ -1,11 +1,28 @@
 import http from 'k6/http';
+import { check, sleep } from 'k6';
+import { Rate } from 'k6/metrics';
+
+const fallbackRate = new Rate('fallback_triggered');
 
 export const options = {
-    vus: 300000,
-    duration: '30s',
+    vus: 1,
+    iterations: 20,
 };
 
+const BASE_URL = 'http://localhost:8080/api/call?slow=true';
+
 export default function () {
-    const res = http.get('http://localhost:8080/api/pay');
-    console.log(`Status: ${res.status}`);
+    let res = http.get(BASE_URL);
+
+    check(res, {
+        'is status 200': (r) => r.status === 200,
+    });
+
+    const isFallback = res.body.includes('Fallback');
+    fallbackRate.add(isFallback);
+
+    // Виводимо статус та повністю тіло відповіді (body), яке прийшло від сервера
+    console.log(`[Ітерація ${__ITER + 1}] Статус: ${res.status} | Відповідь: ${res.body}`);
+
+    sleep(0.5);
 }
